3211e4159b1123f048993f3f28e7875b7ee51356
Gerald <geraldenyenwa@live.com>
2023-03-17 16:03:41 +0000

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Programing prints all char except e & q
+ *
+ * Return: Outputs char without e & q
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == 'e' || i == 'q')
+			i == ' ';
+		else
+			putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
