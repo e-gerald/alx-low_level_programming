fd86fc896e69767018809438caf7f290ea631ed3
Gerald <geraldenyenwa@live.com>
2023-03-17 12:10:07 +0000

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Prints a - z alphabets
+ * Return: small alphabets
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar (i);
+	return (0);
+}
