62f620890f082c7f6865ae8d80452ebd4993f029
Gerald <geraldenyenwa@live.com>
2023-03-18 10:31:19 +0000

    11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+/**
+ * main - Print 3 combination
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (k = 0; k < 10; k++)
+			{
+
+				if (i < j && i < k && j < k && i != j && i != k && j != k)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+					if (i + j + k != 24)
+					putchar(',');
+					putchar(' ');
+				}
+				else
+					continue;
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
