496d0ac9223b7895dd70efe471f75b033601f190
Gerald <geraldenyenwa@live.com>
2023-03-18 20:27:47 +0000

    There

---
+++ 0x00-hello_world/test.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int sumofnumbers(int a, int b)
+{
+	int i, j, sum = 0;
+	sum = i + j;
+	return (sum)
+}
+
+int main ()
+{
+	int a = 4;
+	int b = 6;	
+	sumofnumbers(a, b);
+	printf("%d", sum)
+	return 0;
+}
