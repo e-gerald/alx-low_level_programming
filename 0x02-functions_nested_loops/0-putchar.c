b07acfce57b8f36667517c37239ace3dbc2fc76f
Gerald <geraldenyenwa@live.com>
2023-03-21 14:23:56 +0000

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,7 @@
  * Return: Always 0
  */
 
-void main(void)
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -16,4 +16,5 @@ void main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+	return (0);
 }
