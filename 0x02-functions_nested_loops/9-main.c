4624c3be7922c0462334d54ec542f5bc14d08422
Gerald <geraldenyenwa@live.com>
2023-03-22 20:17:54 +0000

    9

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  * main - check the code
+ *   *
+ i*    * Return: Always 0.
+ *     */
+
+void times_table(void);
+int _putchar(char c);
+
+int main(void)
+{
+	    times_table();
+	        return (0);
+}
