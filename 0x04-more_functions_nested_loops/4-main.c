1df0bf5d4aff3649c0bd59562a6572d52117a860
Gerald <geraldenyenwa@live.com>
2023-03-23 23:25:25 +0000

    4

---
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  * main - check the code
+ *   *
+ *    * Return: Always 0.
+ *     */
+
+int _putchar(char c);
+
+void print_most_numbers(void);
+
+int main(void)
+{
+	    print_most_numbers();
+	        return (0);
+}
