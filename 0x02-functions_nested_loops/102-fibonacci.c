af8821171173cd1434f73bf65ab6b131657a4144
Gerald <geraldenyenwa@live.com>
2023-03-23 04:08:39 +0000

    13

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Print fibonacci sequence
+ * Return: Always 0;
+ */
+
+int main(void)
+{
+	long int i = 0, j = 1, k = 0, n;
+
+	k = i + j;
+	printf("%ld, ", k);
+	for (n = 0; n < 49; n++)
+	{
+		printf("%ld, ", k);
+		i = j;
+		j = k;
+		k = i + j;
+	}
+	printf("2036501107\n");
+	return (0);
+}
