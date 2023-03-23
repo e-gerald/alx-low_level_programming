1a674e215f02d0d67bf10e5cbb0f4eb6e101abc1
Gerald <geraldenyenwa@live.com>
2023-03-23 04:49:31 +0000

    15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 
 /**
- * main - Prints even fibonacci sequence
- * Return: Always 0
+ * main - Print even fibonacci sequence
+ * Return: Always 0;
  */
 
 int main(void)
 {
-	int 
+	long int i = 0, j = 1, k = 0, n;
+
+	k = i + j;
+	for (n = 0; n < 50; n++)
+	{
+		i = j;
+		j = k;
+		k = i + j;
+		if (k < 3524578 && k % 2 == 0)
+		{
+			printf("%ld, ", k);
+		}
+	}
+	printf("3524578\n");
+	return (0);
+}
