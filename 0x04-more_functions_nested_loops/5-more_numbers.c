fd02e064d126ac282c15a0b9b5daeac111bdf2d3
Gerald <geraldenyenwa@live.com>
2023-03-24 19:29:39 +0000

    7

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,20 +7,23 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int x, y;
 
-	for (j = 0; j < 10; j++)
+	if (n <= 0)
 	{
-		for (i = 0; i < 15; i++)
-		{
-			if (i > 9)
-			{
-				_putchar(i / 10 + '0');
-				_putchar(i % 10 + '0');
-			}
-			else if (i <= 9)
-				_putchar(i + '0');
-		}
 		_putchar('\n');
 	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			for (y = 0; y < x; y++)
+			{
+				_putchar('\\');
+			}
+			_putchar(' ');
+			_putchar('\n');
+		}
+	}
 }
+
