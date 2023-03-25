99d0875f33586d8c928c635e2be7c6ea8fcf3d36
Gerald <geraldenyenwa@live.com>
2023-03-25 00:41:08 +0000

    2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,9 +9,10 @@
  */
 
 int largest_number(int a, int b, int c)
+
 {
 	int largest;
-	
+
 	if (a > b && a > c)
 	{
 		largest = a;
@@ -24,6 +25,5 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-
 	return (largest);
 }
