28e26f88d58d29be998c18daa9558fd07e1aed93
Gerald <geraldenyenwa@live.com>
2023-03-28 05:28:15 +0000

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,15 +14,9 @@ void puts_half(char *str)
 	n = (strlen(str) - 1) / 2;
 	for (i = 0; i <= n; i++)
 	{
-		if (n % 2 != 0 && n < 10)
-		{
-			printf("%c", str[n + i]);
-		}
-		else if (n % 2 == 0 && n < 10)
-		{
+		if (n % 2 == 0 && n < 10)
 			printf("%c", str[n + i + 1]);
-		}
-		else
+		else if (i != 0)
 			printf("%c", str[n + i]);
 	}
 	printf("\n");
