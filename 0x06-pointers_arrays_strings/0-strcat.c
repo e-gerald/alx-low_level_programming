f0d3a4bacf472d565c8700d6750c92e3caca5703
Gerald <geraldenyenwa@live.com>
2023-03-29 09:09:33 +0000

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strcat - Concatenates two strings
+ * @dest: Argument 1
+ * @src: Argument 2
+ * Return: Always 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
