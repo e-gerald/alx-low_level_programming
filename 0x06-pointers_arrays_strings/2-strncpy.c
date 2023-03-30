b88593e0002f0155411ae8b1a53bbd6e0e80dd6b
Gerald <geraldenyenwa@live.com>
2023-03-30 15:23:28 +0000

    2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,15 +1,19 @@
+#include <stdio.h>
 #include "main.h"
-#include <string.h>
-
-/**
- * _strncpy - Copy string to a destination
- * @dest: Argument 1
- * @src: Argument 2
- * @n: Size
- * Return: Always 0
- */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	return (strncpy(dest, src, n));
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
 }
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * *leet - Replace letters with digit
+ * @s: Argument
+ * Return: Always 0
+ */
+
+char *leet(char *)
+{
+	char 
