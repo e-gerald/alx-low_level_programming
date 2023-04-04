ad6514ea474c7996194ca5572f3e7a169f4b5e28
Gerald <geraldenyenwa@live.com>
2023-04-04 00:39:52 +0000

    3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -16,7 +16,7 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (s[i] == accept[j])
+			if (s[i] == accept[j] && s[i] != '\0' && accept[j - 1] != '\0')
 			{
 				sum += sizeof(s[i]);
 			}
