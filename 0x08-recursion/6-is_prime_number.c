0ad4c280d37ea38e62b44c9c3b4e8c0642d3cc83
Gerald <geraldenyenwa@live.com>
2023-04-06 01:51:05 +0000

    6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,12 +2,40 @@
 
 /**
  * is_prime_number - Find prime number
- * @n: Argument 
+ * @n: Argument
+ * @i: Iterator
  * Return: 1 and 0
  */
 
+int prime(int n, int i);
+
 int is_prime_number(int n)
 {
-	if (n % i == 0)
-	{
+	int i = 0;
 
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (prime(n, i + 1));
+}
+
+/**
+ * prime - Find prime number
+ * @n: Number argument
+ * @i: Iterator
+ * Return: Prime sighals
+ */
+
+int prime(int n, int i)
+{
+	if (i > 1 && i < n && n % i == 0)
+	{
+		return (0);
+	}
+	else if (i > 1 && i < n && n % i != 0)
+	{
+		return (1);
+	}
+	return (prime(n, i + 1));
+}
