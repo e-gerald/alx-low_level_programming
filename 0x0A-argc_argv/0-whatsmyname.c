dafb9779ff8de336e040371f188c39e7c8f8542b
Gerald <geraldenyenwa@live.com>
2023-04-08 10:12:19 +0000

    Print file name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,13 +8,8 @@
  * Return: File name
  */
 
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
+	printf("%s\n", argv[0]);
 	return (0);
 }
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints number of argument
+ * @argc: Argument counter
