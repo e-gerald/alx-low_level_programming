a5361ab1ad87b8fe830c088a3d8500b6244c4aef
Gerald <geraldenyenwa@live.com>
2023-04-10 05:29:38 +0000

    Minimal number of coins for change

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,7 +1,3 @@
 int _putchar(char c);
 int main(int argc, char *argv[]);
 int main(int argc, char __attribute__((__unused__)) *argv[]);
-int main(int argc, char *argv[]);
-int main(int argc, char *argv[]);
-int main(int argc, char *argv[]);
-int main(int argc, char *argv[]);
