048d3d5f905e0de60eca3b55106580d4606ebd75
Gerald <geraldenyenwa@live.com>
2023-04-10 23:03:47 +0000

    Add positive numbers from the command line

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * main - Add positive numbers
@@ -11,23 +12,32 @@
 
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
-	char **c = argv;
+	int i;
+	unsigned int k, sum = 0;
+	char *e;
 
-	if (argc == 1)
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			e = argv[i];
+			
+			for (k = 0; k < strlen(e); k++)
+			{
+				if (e[k] < 48 || e[k] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(e);
+			e++;
+		}
+		printf("%d\n", sum);
+	}
+	else
 	{
 		printf("0\n");
-		return (0);
 	}
-	for (i = 0; *c[i] != '\0'; i++)
-	{
-		if ((*c[i] < 48) || (*c[i] > 57))
-		{
-			printf("Error\n");
-			return (1);
-		}
-		sum += atoi(c[i]);
-	}
-	printf("%d\n", sum);
 	return (0);
 }
