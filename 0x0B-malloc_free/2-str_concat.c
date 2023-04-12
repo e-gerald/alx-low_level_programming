2045bcc77655c1bee4784fbe4a53b05c85668042
Gerald <geraldenyenwa@live.com>
2023-04-12 20:36:54 +0000

    Concatenate two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,15 +15,8 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 		s1 = "";
-	else if (s2 == NULL)
-	{
+	if (s2 == NULL)
 		s2 = "";
-	}
-	else if (s1 == NULL && s2 == NULL)
-	{
-		s1 = "";
-		s2 = "";
-	}
 	while (s1[i] != '\0')
 	{
 		i++;
@@ -48,5 +41,6 @@ char *str_concat(char *s1, char *s2)
 		c[i] = s2[j];
 		i++, j++;
 	}
+	c[i] = '\0';
 	return (c);
 }
