77bf781439a9011af98a86b7dbe5c2e12726df6f
Gerald <geraldenyenwa@live.com>
2023-04-12 23:50:54 +0000

    Allocate pointer to 2 dimensional array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Pointer to a 2 dimensional array
+ * @width: Argument 1
+ * @height: Argument 2
+ * Return: Return address of pointer
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j, **s;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	s = malloc(height * sizeof(int *));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		s[i] = malloc(width * sizeof(int));
+		{
+			if (s[i] == NULL)
+			{
+				for (; i >= 0; i--)
+					free(s[i]);
+			}
+			free(s);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			s[i][j] = 0;
+		}
+	}
+	return (s);
+}
