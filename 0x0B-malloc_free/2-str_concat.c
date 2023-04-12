10b4344771cebcc59175ba26cb7f92928be1c49a
Gerald <geraldenyenwa@live.com>
2023-04-12 01:05:18 +0000

    Concatenate a string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,7 +21,7 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
-	c = malloc((sizeof(s1) + sizeof(s2)) * sizeof(char));
+	c = malloc((sizeof(s1) + sizeof(s2) + 1) * sizeof(char));
 	if (c == NULL)
 		return (NULL);
 	i = j = 0;
