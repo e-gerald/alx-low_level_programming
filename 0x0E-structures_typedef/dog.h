0417c76700552495cfc03f10a60cce3b1bded9fc
Gerald <geraldenyenwa@live.com>
2023-04-18 04:46:54 +0000

    Struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - Struct data-type
+ * @name: Datatype 1
+ * @age: Datatype 2
+ * @owner: Datatype 3
+ * Description: Read the code
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
