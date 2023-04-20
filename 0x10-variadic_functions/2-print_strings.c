2dab50e5c9dccec2ce7aa44e60111bc9f9e234bf
Gerald <geraldenyenwa@live.com>
2023-04-20 23:38:35 +0000

    Print string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("%s", separator);
 		}
-		else if (i != (n - 1) && separator == NULL)
+		else if (i != (n - 1) && va_arg(string, char *) == NULL)
 		{
 			printf("(nil)");
 		}
