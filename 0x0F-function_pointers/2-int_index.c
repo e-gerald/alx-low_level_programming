750eef1a9194763fec0600977942f753f569eb6d
Gerald <geraldenyenwa@live.com>
2023-04-20 01:21:08 +0000

    Compare for similar integers

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - Check for equal integers
+ * @array: Integer array
+ * @size: Size of array
+ * @cmp: Compare function
+ * Return: First index of equal array
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		{
+			return (i);
+		}
+	}
+	return (0);
+}
