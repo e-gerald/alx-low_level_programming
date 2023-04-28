03e8ed42bcb183f753a4893add34187afcc7a7fa
Gerald Enyenwa <geraldenyenwa@live.com>
2023-04-28 01:25:06 +0000

    Printf before main using the contructor attribute

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * print_poem - Print a poem before the main function is called
+ * Return: A poem from print_poem()
+ */
+
+void print_poem(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
