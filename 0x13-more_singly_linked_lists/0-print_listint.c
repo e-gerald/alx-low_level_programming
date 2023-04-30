8dc5c9380f013bac14f3b16cb3e18684cf23bc68
Gerald Enyenwa <geraldenyenwa@live.com>
2023-04-30 22:31:04 +0000

    Print all int element of a singly linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,11 +12,12 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-		if (h->n == '\0')
-			return (0);
-		printf("%d\n", h->n);
-		h = h->next;
-		count++;
+		if (h->n != '\0')
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+			count++;
+		}
 	}
 	return (count);
 }
