ccfd60d59df3883a58fcf352354bbc75356434a3
Gerald Enyenwa <geraldenyenwa@live.com>
2023-05-01 21:43:09 +0000

    Write and pop singly linked list

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,7 +11,7 @@ int pop_listint(listint_t **head)
 	listint_t *temp;
 	int value;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (0);
 	temp = (*head)->next;
 	value = (*head)->n;
