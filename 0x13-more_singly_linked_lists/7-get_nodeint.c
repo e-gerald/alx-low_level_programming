6287898867d71668a8b6b9869bc189e5d614a039
Gerald Enyenwa <geraldenyenwa@live.com>
2023-05-01 22:07:42 +0000

    Return the nth element of a singly linked list

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - Find nth node of a singly linked list
+ * @head: Head node
+ * @index: nth element
+ * Return: value of nth element
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *temp = head;
+
+	if (temp != NULL && i < index)
+	{
+		temp = head->next;
+		i++;
+	}
+	return (temp ? temp : NULL);
+}
