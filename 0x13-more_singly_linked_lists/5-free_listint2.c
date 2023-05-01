d65d9b963ad5b4ff050754bd96a4e70311812424
Gerald Enyenwa <geraldenyenwa@live.com>
2023-05-01 21:02:35 +0000

    Free node and assign head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Free memory and assign head to NULL
+ * @head: Head of node
+ * Return: Nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	*head = NULL;
+}
+
