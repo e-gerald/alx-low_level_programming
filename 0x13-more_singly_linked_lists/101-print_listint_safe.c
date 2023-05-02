21d41abb08f6516e928e5d4ae9f9f5b18eac44bf
Gerald Enyenwa <geraldenyenwa@live.com>
2023-05-02 04:07:55 +0000

    Print list in a linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -58,7 +58,6 @@ size_t print_listint_safe(const listint_t *head)
 			printf("[%p] %d\n", (void *)head, head->n);
 			head = head->next;
 		}
-		printf("-> [%p] %d\n", (void *)head, head->n);
 	}
 	else
 	{
